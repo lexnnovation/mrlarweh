c40c6d1375b4386f41026d41f85a670a02e567c4
Alexander Teye <alexander.teye29@gmail.com>
2024-10-30 21:09:17 +0000

    Bio

---
+++ src/components/Bio.jsx
@@ -0,0 +1,10 @@
+const Bio = () => {
+  return (
+    <section className='flex flex-col max-w-4xl gap-12 pt-20' id='bio'>
+      <h2>About Me</h2>
+      <div></div>
+    </section>
+  );
+};
+
+export default Bio;
