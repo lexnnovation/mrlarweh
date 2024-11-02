5297decb5b5c85b697f6e586014a080a6ead6126
Alexander Teye <alexander.teye29@gmail.com>
2024-11-02 21:11:48 +0000

    Added skills section

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -2,7 +2,7 @@ import { BIO } from "../constants";
 
 const Bio = () => {
   return (
-    <section className='flex flex-col max-w-4xl gap-12 pt-20' id='bio'>
+    <section className='flex flex-col max-w-4xl gap-12 pt-20 max' id='bio'>
       <h2 className='text-3xl text-center lg:text-4xl'>About Me</h2>
       <div>
         {BIO.map((bio, index) => (
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 const Skills = () => {
   return (
-    <section>
-      <div></div>
+    <section id='skills'>
+      <div className='container mx-auto'>Ghanba</div>
     </section>
   );
 };
