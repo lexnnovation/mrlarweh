c16361a85337cb002011225f4bb654f9b094fd91
Alexander Teye <alexander.teye29@gmail.com>
2024-11-19 16:52:50 +0000

    Adding the Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,11 +3,14 @@ import { EDUCATION } from "../constants";
 
 const Education = () => {
   return (
-    <section>
-      <h2>Education</h2>
+    <section className='py-8' id='education'>
+      <h2 className='mb-4 text-4xl font-bold text-center'>Education</h2>
       {EDUCATION.map((edu, index) => (
         <div key={index} className='p-10 mb-6'>
-          h3
+          <h3 className='text-xl font-semibold'>{edu.degree}</h3>
+          <p className='text-lg '>{edu.institution}</p>
+          <p className='text-sm text-stone-300'>{edu.duration}</p>
+          <p className='mt-2'>{edu.description}</p>
         </div>
       ))}
     </section>
