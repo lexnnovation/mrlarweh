e31a027e9c6d98f6f93b27e6939a2d9035eeb279
Alexander Teye <alexander.teye29@gmail.com>
2024-11-23 11:38:49 +0000

    update education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,7 +6,10 @@ const Education = () => {
     <section className='py-8' id='education'>
       <h2 className='mb-4 text-4xl font-bold text-center'>Education</h2>
       {EDUCATION.map((edu, index) => (
-        <div key={index} className='p-10 mb-6'>
+        <div
+          key={index}
+          className='p-4 mb-6 border rounded-xl border-stone-50/30'
+        >
           <h3 className='text-xl font-semibold'>{edu.degree}</h3>
           <p className='text-lg '>{edu.institution}</p>
           <p className='text-sm text-stone-300'>{edu.duration}</p>
